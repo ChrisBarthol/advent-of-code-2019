eaa7d6222f285b4c19c6156dea997b3af9c03969
Chris Barthol <chris.barthol@gmail.com>
2019-12-02 13:26:54 -0500

    switch to using a function to calculate fuel

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -11,7 +11,12 @@ fn main() {
 
     let split = contents.lines();
 
-    let sum = split.fold(0.0, |sum, s| sum + (s.parse::<f64>().unwrap()/3.0).floor()-2.0);
+    let sum = split.fold(0.0, |sum, s| sum + fuel_needed_for(s.parse::<f64>().unwrap()));
 
     println!("number {}", sum);
+}
+
+
+fn fuel_needed_for(item: f64) -> f64 {
+    (item/3.0).floor()-2.0
 }
