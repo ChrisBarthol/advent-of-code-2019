4c6db2837c5a245b754a066763cae1d8d8eea8ff
Chris Barthol <chris.barthol@gmail.com>
2019-12-02 16:51:39 -0500

    complete part 2 of day 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
 
     let split = contents.lines();
 
-    let sum = split.fold(0.0, |sum, s| sum + fuel_needed_for(s.parse::<f64>().unwrap()));
+    let sum = split.fold(0.0, |sum, s| sum + recursive_fuel_needed_for(s.parse::<f64>().unwrap()));
 
     println!("number {}", sum);
 }
@@ -21,11 +21,37 @@ fn fuel_needed_for(item: f64) -> f64 {
     (item/3.0).floor()-2.0
 }
 
+fn recursive_fuel_needed_for(item: f64) -> f64 {
+    let mut v: Vec<f64> = Vec::new();
+    let mut fuel = fuel_needed_for(item);
+    v.push(fuel);
+    while fuel >= 0.0 {
+        fuel = fuel_needed_for(fuel);
+        if fuel >= 0.0 {
+            v.push(fuel);
+        }
+    }
+    let sum: f64 = v.iter().sum();
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn it_works() {
         assert_eq!(2.0, fuel_needed_for(14.0));
+    }
+    #[test]
+    fn recursive_fuel_for_14() {
+        assert_eq!(2.0, recursive_fuel_needed_for(14.0));
+    }
+    #[test]
+    fn recursive_fuel_for_1969() {
+        assert_eq!(966.0, recursive_fuel_needed_for(1969.0));
+    }
+    #[test]
+    fn recursive_fuel_for_100756() {
+        assert_eq!(50346.0, recursive_fuel_needed_for(100756.0));
     }
 }
